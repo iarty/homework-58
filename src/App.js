eb935e8df85176818af0bcc1377538866892d5ce
Barbonov Artem <abarbonov@gmail.com>
2019-12-02 17:25:11 +0600

    finally project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import classes from './App.module.css'
+import Modal from './components/UI/Modal/Modal';
+import Alert from './components/UI/Alert/Alert';
+import Button from './components/UI/Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  state = {
+    showModal: false,
+    showAlert: true
+  };
+
+  ModalHandler = () => {
+    this.setState({ showModal: !this.state.showModal })
+  }
+
+  Continued = () => {
+    alert('Continued')
+  }
+
+  AlertHandler = () => {
+    this.setState({ showAlert: false })
+  }
+
+  render() {
+    const button = [
+      { type: 'Primary', label: 'Continue', clicked: this.Continued },
+      { type: 'Danger', label: 'Close', clicked: this.ModalHandler }
+    ]
+    return (
+      <Fragment>
+        <Modal
+          show={this.state.showModal}
+          close={this.ModalHandler}
+          title="Some kinda modal title"
+          buttonType={this.state.button}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum deserunt et unde, perferendis sunt dolorum corporis velit itaque libero! Hic necessitatibus quos soluta unde inventore? Voluptatem a sit explicabo ad, amet unde accusantium ullam?</p>
+          <div className={classes.ButtonWrap}>
+            <hr />
+            {button.map((el, i) => <Button key={i + el.label} type={el.type} clicked={el.clicked}>{el.label}</Button>)}
+          </div>
+        </Modal>
+        <div className={classes.Container}>
+          <Alert
+            // clickDismissable
+            type='Warning'
+            show={this.state.showAlert}
+            dismiss={this.AlertHandler}
+          >
+            This is a warning type alert
+          </Alert>
+
+          <div className={classes.ButtonInner}>
+            <Button type='Modal' clicked={this.ModalHandler}>Show Modal</Button>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
-export default App;
